81500bf2e4a665b91bb96ab14e975aeda6326092
sangram-dotnet-pune <sangramcanva7812@gmail.com>
2025-10-08 16:07:23 +0530

    Add project files.

---
+++ DotnetTask2_WEBAPI1_Collection_Console/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetTask2_WEBAPI1_Collection_Console
+{
+    internal class Employee
+    {
+        [Required(ErrorMessage = "ID is mandatory")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 30 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Department is required")]
+        public string Department { get; set; }
+
+        [Required(ErrorMessage = "Mobile number is required")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Mobile number must be exactly 10 digits")]
+        public string MobileNo { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+    }
+}
---
+++ DotnetTask2_WEBAPI1_Collection_Console/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using DemoWebAPI_ClientDemo;
+using DotnetTask2_WEBAPI1_Collection_Console;
+
+namespace DemoWebAPI_ClientDemo
+
+{
+
+    internal class Program
+
+    {
+
+        static async Task Main(string[] args)
+
+        {
+
+            // Base URL of the ASP.NET Core Web API
+
+            string apiBaseUrl = " http://localhost:5103/api/Employee"; //you need to change url according to your web api
+
+            HttpClient client = new HttpClient();
+
+            try
+
+            {
+
+                Console.WriteLine("Fetching products from the Web API...");
+
+                // Make an HTTP GET request
+
+                List<Employee> Emps = await client.GetFromJsonAsync<List<Employee>>(apiBaseUrl);
+
+                if (Emps != null && Emps.Count > 0)
+
+                {
+
+                    //Console.WriteLine("List of Products from SQL Server Products Table:");
+
+                    Console.WriteLine("List of Products from Products Collection:");
+
+                    foreach (var e in Emps)
+
+                    {
+
+                        Console.WriteLine("Product ID:" + e.Id + ",  Name:" + e.Name + "  Department:" + e.Department+" Mobile No:-"+e.MobileNo+" EMail:-"+e.Email);
+
+                    }
+
+                }
+
+                else
+
+                {
+
+                    Console.WriteLine("No products found.");
+
+                }
+
+            }
+
+            catch (HttpRequestException ex)
+
+            {
+
+                Console.WriteLine("Exception from API:" + ex.Message);
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                Console.WriteLine("Unknown Exception Occurred..!!:" + ex.Message);
+
+            }
+
+        }
+
+    }
+
+}
+
